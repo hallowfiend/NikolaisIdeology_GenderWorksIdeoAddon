d3bb64293bf5e0ad7969d72012aa67c64e4f7573
Nikolai Jakobslock <hallowfiend@protonmail.com>
2025-09-07 10:59:29 +0200

    harmony

--- Source/NikolaisIdeology_GenderWorks_ThoughtWorker_NeedBeauty_Patch.cs
+++ Source/NikolaisIdeology_GenderWorks_ThoughtWorker_NeedBeauty_Patch.cs
@@ -1,11 +1,6 @@
-﻿using HarmonyLib;
-using RimWorld;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RimWorld;
 using Verse;
+using HarmonyLib;
 
 namespace NikolaisIdeology.GenderWorks;
 
