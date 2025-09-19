58ed92f36184f2cf705f2d4e12bb5910a8e3718d
Nikolai Jakobslock <hallowfiend@protonmail.com>
2025-09-19 12:46:00 +0200

    stuff

--- Source/RitualOutcomeComp_PawnsAreLovers.cs
+++ Source/RitualOutcomeComp_PawnsAreLovers.cs
@@ -15,22 +15,6 @@ namespace NikolaisIdeology_GenderWorks
             return true;
         }
 
-        public override float Count(LordJob_Ritual ritual, RitualOutcomeComp_Data data)
-        {
-            Pawn pawn1 = ritual.assignments.FirstAssignedPawn("pawn1");
-            Pawn pawn2 = ritual.assignments.FirstAssignedPawn("pawn2");
-            List<DirectPawnRelation> directRelations = (List<DirectPawnRelation>)PawnRelationUtility.GetRelations(pawn1, pawn2);
-
-            foreach (DirectPawnRelation directRelation in directRelations)
-            {
-                if (directRelation.def == PawnRelationDefOf.Lover && directRelation.otherPawn == pawn2)
-                    return 1;
-                else
-                    return 0f;
-            }
-            return 0f;
-        }
-
         public override QualityFactor GetQualityFactor(Precept_Ritual ritual, TargetInfo ritualTarget, RitualObligation obligation, RitualRoleAssignments assignments, RitualOutcomeComp_Data data)
         {
             float quality = 0f;
--- Source/RitualPosition_Spouse1.cs
+++ Source/RitualPosition_Spouse1.cs
@@ -1,10 +1,34 @@
 using RimWorld;
+using System;
 using Verse;
 namespace NikolaisIdeology_GenderWorks
 {
     public class RitualPosition_Spouse1 : RitualPosition_VerticalThingCenter
     {
 
+        public override PawnStagePosition GetCell(IntVec3 spot, Pawn p, LordJob_Ritual ritual)
+        {
+            Thing thing = spot.GetThingList(p.Map).FirstOrDefault<Thing>((Predicate<Thing>)(t => t == ritual.selectedTarget.Thing));
+            CellRect cellRect = thing != null ? thing.OccupiedRect() : CellRect.SingleCell(spot);
+            Map mapHeld = p.MapHeld;
+            CellRect rect = this.GetRect(cellRect);
+            Func<IntVec3, bool> Validator = CommonRitualCellPredicates.DefaultValidator(spot, mapHeld, p, cellRect);
+            if (Validator(rect.CenterCell))
+                return new PawnStagePosition(rect.CenterCell, thing, Rot4.East, this.highlight);
+            IntVec3 cell = IntVec3.Invalid;
+            for (int index = 0; index < 16 /*0x10*/ && index < rect.Width; ++index)
+            {
+                IntVec3 randomCell = rect.RandomCell;
+                if (Validator(randomCell))
+                {
+                    cell = randomCell;
+                    break;
+                }
+            }
+            if (!cell.IsValid)
+                cell = this.GetFallbackSpot(cellRect, spot, p, ritual, Validator);
+            return new PawnStagePosition(cell, thing, Rot4.East, this.highlight);
+        }
         protected override CellRect GetRect(CellRect thingRect)
         {
             IntVec3 intVec3 = IntVec3.SouthWest + this.offset;
