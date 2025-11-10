4200d26717a4f3f6602e2a26edd695d31e2b12cc
Nikolai Jakobslock <hallowfiend@protonmail.com>
2025-11-10 09:37:51 +0100

    fixing references and updating oathspouse for new intimacy version

--- Source/NikolaisIdeology_GenderWorks_Lovin_Patch.cs
+++ Source/NikolaisIdeology_GenderWorks_Lovin_Patch.cs
@@ -1,15 +1,15 @@
-﻿using HarmonyLib;
-using LoveyDoveySexWithEuterpe;
+﻿using LoveyDoveySexWithEuterpe;
 using RimWorld;
 using Verse;
+using HarmonyLib;
 
 namespace NikolaisIdeology_GenderWorks
 {
-    [HarmonyPatch(typeof(SexUtilities), "CanDoLovinAtAll")]
+    [HarmonyPatch(typeof(SexUtilities), "CanEverDoLovin")]
     public static class NikolaisIdeology_GenderWorks_Lovin_Patch
     {
-        [HarmonyPrefix]
-        public static void Prefix(Pawn pawn, ref bool __result)
+        [HarmonyPostfix]
+        public static void Postfix(Pawn pawn, ref bool __result)
         {
             if (pawn != null && pawn.Ideo != null)
             {
